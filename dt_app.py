e38b0b4ab8562993bfed5bb29da442a07446aa7e
DIXIT <107348377+DIXIT0043@users.noreply.github.com>
2024-10-04 17:09:55 +0530

    Update dt_app.py

--- dt_app.py
+++ dt_app.py
@@ -22,7 +22,7 @@ input_df = pd.DataFrame(input_data)
 encoder = LabelEncoder()
 input_df['Gender'] = encoder.fit_transform(input_df['Gender'])
 
-with open(r'C:\Users\kumar\OneDrive\Desktop\Machine Learning\Jupyter\DL_Classroom\Learning\Obesity_detector\obesity_model.h5', 'rb') as file:
+with open(r'obesity_model.h5', 'rb') as file:
     model = pickle.load(file)
 
 prediction = model.predict(input_df)
